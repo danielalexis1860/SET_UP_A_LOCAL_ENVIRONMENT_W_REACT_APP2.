fd2ab25fc3cf5afe5d77a512d2465b666999c260
danielferguson <107422624+danielalexis1860@users.noreply.github.com>
2024-02-26 07:36:54 -0800

    Add files via upload

---
+++ BUILD A REACT INFO SITE/SETUP A LOCAL ENVIRONMENT W  CREATE REACT APP/COMPONENTS/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main(){
+    return(
+        <main>
+            <h1 className = "main--title">Fun Facts about React</h1>
+            <ul className = "main--facts">
+                <li>Me just being great</li>
+                <li>I've done it all</li>
+                <li>The Lord bless y'all</li>
+                <li>Play or do better</li>
+            </ul>
+        </main>
+    )
+}
+
